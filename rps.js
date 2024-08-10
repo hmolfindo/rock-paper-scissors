ae0a9ba11c1279ba0194f6ba8fcc3f818dceb9e9
Milo Olfindo <142918074+hotcake1@users.noreply.github.com>
2024-08-11 00:06:20 +0800

    Add scenario for ending game

--- rps.js
+++ rps.js
@@ -19,76 +19,112 @@ function playGame(){
     const score = document.getElementById("scoreboard");
 
     rock.addEventListener("click", function(){
-        console.log("rock");
         playRound("rock", getComputerChoice());
     });
     paper.addEventListener("click", function(){
-        console.log("paper");
         playRound("paper", getComputerChoice());
     });
     scissors.addEventListener("click", function(){
-        console.log("scissors");
         playRound("scissors", getComputerChoice());
     });
 
+    function deleteButtons(){
+        rock.remove();
+        paper.remove();
+        scissors.remove();
+    };
+
+    function announceWinner(winner){
+        if(winner === "human"){
+            result.textContent = "You have defeated the computer!";
+        }else if(winner === "computer"){
+            result.textContent = "You have been defeated by the computer!";
+        }
+    };
+
     let humanScore = 0;
     let computerScore = 0;
+
     //Start round
     function playRound(humanChoice, computerChoice){
-        if(humanScore < 4 && computerScore < 4){
-            //Rock cases
-            if (humanChoice === "rock"){
-                if(computerChoice === "paper"){
-                    computerScore++;
+        //Rock cases
+        if (humanChoice === "rock"){
+            if(computerChoice === "paper"){
+                computerScore++;
+                score.textContent = "You: " + humanScore + " Computer: " + computerScore;
+                if(computerScore < 5 && humanScore < 5){
                     result.textContent = "You lose! Paper beats rock!";
-                    score.textContent = "You: " + humanScore + " Computer: " + computerScore;
-                }else if(computerChoice === "scissors"){
-                    humanScore++;
+                }else{
+                    deleteButtons();
+                    if(humanScore === 5) announceWinner("human");
+                    else announceWinner("computer");
+                }
+            }else if(computerChoice === "scissors"){
+                humanScore++;
+                score.textContent = "You: " + humanScore + " Computer: " + computerScore;
+                if(computerScore < 5 && humanScore < 5){
                     result.textContent = "You win! Rock beats scissors! ";
-                    score.textContent = "You: " + humanScore + " Computer: " + computerScore;
                 }else{
-                    result.textContent = "It's a draw!";
-                    score.textContent = "You: " + humanScore + " Computer: " + computerScore;
+                    deleteButtons();
+                    if(humanScore === 5) announceWinner("human");
+                    else announceWinner("computer");
                 }
-            } //Paper cases
-            else if(humanChoice === "paper" ){
-                if(computerChoice === "scissors"){
-                    computerScore++;
-                    result.textContent = "You lose! ";
-                    score.textContent = "You: " + humanScore + " Computer: " + computerScore;
-                }else if(computerChoice === "rock"){
-                    humanScore++;
-                    result.textContent = "You win! ";
-                    score.textContent = "You: " + humanScore + " Computer: " + computerScore;
-                }else{
-                    result.textContent = "It's a draw! ";
-                    score.textContent = "You: " + humanScore + " Computer: " + computerScore;
-                }
-            } //Scissors cases
-            else if(humanChoice === "scissors"){
-                if(computerChoice === "rock"){
-                    computerScore++;
-                    result.textContent = "You lose! ";
-                    score.textContent = "You: " + humanScore + " Computer: " + computerScore;
-                }else if(computerChoice === "paper"){
-                    humanScore++;
-                    result.textContent = "You win! ";
-                    score.textContent = "You: " + humanScore + " Computer: " + computerScore;
-                }else{
-                    result.textContent = "It's a draw! ";
-                    score.textContent = "You: " + humanScore + " Computer: " + computerScore;
-                }
-            }
-        }else{
-            if(humanScore === 4){
-                result.textContent = "You've defeated the computer!";
+            }else{
+                result.textContent = "It's a draw!";
                 score.textContent = "You: " + humanScore + " Computer: " + computerScore;
             }
-            else{
-                result.textContent = "You've been defeated by the computer!";
+        } //Paper cases
+        else if(humanChoice === "paper" ){
+            if(computerChoice === "scissors"){
+                computerScore++;
                 score.textContent = "You: " + humanScore + " Computer: " + computerScore;
-            } 
-
+                if(computerScore < 5 && humanScore < 5){
+                    result.textContent = "You lose! Scissors beats paper!";
+                }else{
+                    deleteButtons();
+                    if(humanScore === 5) announceWinner("human");
+                    else announceWinner("computer");
+                }
+            }else if(computerChoice === "rock"){
+                humanScore++;
+                score.textContent = "You: " + humanScore + " Computer: " + computerScore;
+                if(computerScore < 5 && humanScore < 5){
+                    result.textContent = "You win! Paper beats rock!";
+                }else{
+                    deleteButtons();
+                    if(humanScore === 5) announceWinner("human");
+                    else announceWinner("computer");
+                }
+            }else{
+                result.textContent = "It's a draw!";
+                score.textContent = "You: " + humanScore + " Computer: " + computerScore;
+            }
+        } //Scissors cases
+        else if(humanChoice === "scissors"){
+            if(computerChoice === "rock"){
+                computerScore++;
+                score.textContent = "You: " + humanScore + " Computer: " + computerScore;
+                if(humanScore < 5 && computerScore < 5){
+                    result.textContent = "You lose! Rock beats scissors!";
+                }else{
+                    deleteButtons();
+                    if(humanScore === 5) announceWinner("human");
+                    else announceWinner("computer"); 
+                }
+            }else if(computerChoice === "paper"){
+                humanScore++;
+                score.textContent = "You: " + humanScore + " Computer: " + computerScore;
+                if(humanScore < 5 && computerScore < 5){
+                    result.textContent = "You win! Scissors beats paper!";
+                }else{
+                    deleteButtons();
+                    if(humanScore === 5) announceWinner("human");
+                    else announceWinner("computer");
+                }
+            }else{
+                result.textContent = "It's a draw!";
+                score.textContent = "You: " + humanScore + " Computer: " + computerScore;
+            }
         }
     }
         /*//Game will not end until either user score or computer score gets to 5
